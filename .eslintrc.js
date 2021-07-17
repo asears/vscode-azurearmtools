86ac7c547acce945aa7dde1fd7d31ed978b1bd27
asears <asears@users.noreply.github.com>
2021-07-17 05:23:04 -0400

    update linter to ignore quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
-    'google',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
+  extends: ["google"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'rules': {
+  plugins: ["@typescript-eslint", "@typescript-eslint/tslint"],
+  rules: {
+    "@typescript-eslint/quotes": ["off", "single"],
   },
 };
